1b3d4941f0706e9476d18582136fe786c10232a0
EfrosEugeniu-md <64984462+EfrosEugeniu-md@users.noreply.github.com>
2020-05-13 19:45:18 +0300

    Basic use of `<table mat-table>`

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/model/Task';
 import {DataHandlerService} from '../../service/data-handler.service';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-tasks',
@@ -8,16 +10,30 @@ import {DataHandlerService} from '../../service/data-handler.service';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+  // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
+   displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+   dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
   tasks: Task[];
-
-
-  constructor(private dataHandler: DataHandlerService) { }
-
-  ngOnInit(): void {
-    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+  constructor(private dataHandler: DataHandlerService) {
   }
-
-  toglleTaskComplited(task: Task) {
+  ngOnInit() {
+    this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
+    // датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы, JSON и пр.)
+    this.dataSource = new MatTableDataSource();
+    this.refreshTable();
+  }
+  toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+  // в зависимости от статуса задачи - вернуть цвет названия
+   getPriorityColor(task: Task) {
+    if (task.priority && task.priority.color && !task.completed) {
+      return task.priority.color;
+    }
+    return '#fff';
+  }
+  // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
+   refreshTable() {
+    this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
   }
 }
